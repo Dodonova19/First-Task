7d97a445c23d7476dd73e92a344efac896525b13
Dodonova19 <dodonova.vikyss@mail.ru>
2021-09-30 17:46:59 +0800

    Добавлен класс логики

---
+++ First Task/Logic.cs
@@ -0,0 +1,31 @@
+﻿namespace lab_1
+{
+    public class Logic
+    {
+        public static int Solve(int a)
+        {
+            int b = 0;
+            int c = 0;
+            int d = 0;
+
+            b = a % 10;
+            a = a / 10;
+            c = a % 10;
+            a = a / 10;
+            d = a % 10;
+
+            if (b >= c && b >= d)
+            {
+                return b;
+            }
+            else if (c >= b && c >= d)
+            {
+                return c;
+            }
+            else
+            {
+                return d;
+            }
+        }
+    }
+}
